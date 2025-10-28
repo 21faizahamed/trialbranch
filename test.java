bf0e1275baf5255fed68aac43282f4f1ecd81544
21faizahamed <faizahamed2109@gmail.com>
2025-10-28 14:21:30 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,23 @@
 public class test {
+<<<<<<< Updated upstream
     static int a  = 1;
     static int b = 1;
     public static void main(String[] args) {
         if (a == b) {
             System.out.println("This is my test");            
+=======
+    static int a = 1;
+    static int b = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println("Code change test");
+        if (a == b) {
+            System.out.println("This is my test");
+>>>>>>> Stashed changes
         } else {
             System.err.println();
         }
     }
-    
+
 }
