003af59fc589a73dfaeb958597682b172412a9e7
21faizahamed <faizahamed2109@gmail.com>
2025-10-28 14:20:56 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,12 @@
 public class test {
+    static int a  = 1;
+    static int b = 1;
     public static void main(String[] args) {
-        System.out.println("This is my test");
+        if (a == b) {
+            System.out.println("This is my test");            
+        } else {
+            System.err.println();
+        }
     }
     
 }
